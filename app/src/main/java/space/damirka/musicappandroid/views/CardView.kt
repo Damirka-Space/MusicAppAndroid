cf5d74db83ce8839c64e0c45b292188d5fbc630f
Damirka1 <hamitov.damir3101@icloud.com>
2023-09-16 21:27:45 +0600

    Added card view for albums

--- app/src/main/java/space/damirka/musicappandroid/views/CardView.kt
+++ app/src/main/java/space/damirka/musicappandroid/views/CardView.kt
@@ -1,6 +1,18 @@
 package space.damirka.musicappandroid.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import space.damirka.musicappandroid.entities.AlbumEntity
 
@@ -8,8 +20,57 @@ import space.damirka.musicappandroid.entities.AlbumEntity
 fun CardView(
     album: AlbumEntity
 ) {
-    AsyncImage(
-        model = album.image.url,
-        contentDescription = album.title
-    )
+    Box(
+        modifier = Modifier
+            .width(Dp(150f))
+            .height(Dp(190f))
+            .background(
+                color = Color.LightGray,
+                shape = RoundedCornerShape(10.dp)
+            )
+
+    ) {
+        Column {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Box(
+                    modifier = Modifier.width(Dp(140f))
+                        .height(Dp(140f))
+                        .padding(0.dp, 10.dp, 0.dp, 0.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    AsyncImage(
+                        modifier = Modifier.fillMaxSize(),
+                        model = album.image.url,
+                        contentDescription = album.title
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp, 5.dp, 10.dp, 0.dp)
+            ) {
+                Column {
+                    Text(
+                        text = album.title,
+                        style = MaterialTheme.typography.bodyLarge,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Text(
+                        modifier = Modifier.alpha(.8f),
+                        text = album.description,
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/space/damirka/musicappandroid/views/HomeView.kt
+++ app/src/main/java/space/damirka/musicappandroid/views/HomeView.kt
@@ -5,14 +5,13 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import space.damirka.musicappandroid.viewmodels.BlockViewModel
 
 
@@ -26,13 +25,13 @@ fun HomeView(
         contentAlignment = Alignment.Center,
     ) {
         LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(Dp(30f))
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
             items(blocks.data) {
 
                 Box (
                     modifier = Modifier.fillMaxWidth()
-                        .padding(Dp(10f), Dp(30f), Dp(0f), Dp(20f)),
+                        .padding(10.dp, 30.dp, 0.dp, 20.dp),
                     contentAlignment = Alignment.CenterStart
                 ) {
                     Text(
@@ -46,8 +45,8 @@ fun HomeView(
                         state = ScrollState(0),
                         enabled = true
                     )
-                        .padding(Dp(10f), Dp(00f), Dp(10f), Dp(0f)),
-                    horizontalArrangement = Arrangement.spacedBy(Dp(20f))
+                        .padding(10.dp, 0.dp, 10.dp, 0.dp),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
                 ) {
                     it.albums.forEach {
                         CardView(album = it)
